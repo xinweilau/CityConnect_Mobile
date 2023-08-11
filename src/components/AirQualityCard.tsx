8a5114539ed3b30ffa8d687ac7543cdf1b6bfa7a
Lau Xin Wei <xinweii1996@live.com>
2023-08-11 11:16:14 +0800

    fix: overflow card

--- src/components/AirQualityCard.tsx
+++ src/components/AirQualityCard.tsx
@@ -5,7 +5,7 @@ const air = require('../../assets/air-quality.png');
 
 export default function WeatherCard() {
     return (
-        <View style={tw`border border-black border-[#2F6297] border-2 rounded-2xl`}>
+        <View style={tw`border border-[#2F6297] border-2 rounded-2xl`}>
             <View style={tw`absolute left-0 top-0 px-4 py-2 rounded-xl bg-[#2F6297] uppercase`}>
                 <Text style={tw`font-bold text-white`}>Air Quality</Text>
             </View>
@@ -14,12 +14,12 @@ export default function WeatherCard() {
                 <View>
                     <Text style={tw`font-bold text-xl`}>Moderate</Text>
 
-                    <View style={tw`flex flex-row w-full justify-between`}>
+                    <View style={tw`flex flex-row justify-between`}>
                         <Text style={tw`text-lg font-semibold text-gray-700`}>PSI</Text>
                         <Text style={tw`text-lg font-semibold text-gray-700`}>25</Text>
                     </View>
 
-                    <View style={tw`flex flex-row w-full justify-between`}>
+                    <View style={tw`flex flex-row justify-between`}>
                         <Text style={tw`text-lg font-semibold text-gray-700`}>PM2.5</Text>
                         <Text style={tw`text-lg font-semibold text-gray-700`}>10</Text>
                     </View>
